a02744323887c73e718863108fe6ecf0b70761a5
James Byrne <123844389+JamesByrne242@users.noreply.github.com>
2025-11-06 19:21:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# main.py
+import pandas as pd
+import seaborn as sns
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+import numpy as np
+
+#load dataset
+df = pd.read_csv('cybersecurity_intrusion_data.csv')
+
+#identify columns we are working with
+required_cols = ['network_packet_size', 'session_duration']
+
+df_clean = df.dropna(subset=required_cols).copy()
